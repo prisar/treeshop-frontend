d2e3a6e5d7a944d0209face713134615d4dbb8a7
Pritimay Sarkar <pritimaysarkar2010@gmail.com>
2019-04-20 10:37:35 +0530

    take data from input feilds

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
+const initialstate = {
+  loginUser: {
+    email: '',
+    password: ''
+  },
+  users: []
+};
+
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.onSubmit = this.onSubmit.bind(this);
-        this.state ={users: []};
+        //this.onSubmit = this.onSubmit.bind(this);
+        this.state = initialstate;
     }
 
+<<<<<<< HEAD
     onSubmit() {
 <<<<<<< HEAD
         const data = new URLSearchParams("email=abc@abc.com&password=abc@1234");
@@ -17,6 +26,13 @@ class Login extends Component {
         fetch('http://localhost:8000/login', {
             method: 'POST'
 >>>>>>> 30dc8919169fb3a0932192ef16e80c7e0fc84c3a
+=======
+    onSubmit = () => {
+      const data = new URLSearchParams(`email=${this.state.loginUser.email}&password=${this.state.loginUser.password}`);
+        fetch('http://localhost:8000/login', {
+            method: 'POST',
+            body: data
+>>>>>>> take data from input feilds
         }).then((result) => {
             return result.json();
         }).then(users => {
@@ -27,13 +43,24 @@ class Login extends Component {
         });
     }
 
+    fieldChange = (e) => {
+      if (e.target.id === 'email') {
+        const password = this.state.loginUser.password;
+        this.setState({ loginUser: { email: e.target.value, password: password } });
+      } else if (e.target.id === 'password') {
+        const email = this.state.loginUser.email;
+        this.setState({ loginUser: { email: email, password: e.target.value } });
+      }
+      console.log(this.state);
+    }
+
     render() {
       return (
         <div className="Login">
           <h1>Login</h1>
-          <input type="text" placeholder='username'/>
+          <input type="text" id='email' placeholder='email' onChange={this.fieldChange} />
           <br/>
-          <input type='password' placeholder='password' />
+          <input type='password' id='password' placeholder='password' onChange={this.fieldChange} />
           <br/>
           <button onClick={this.onSubmit}>Submit</button>
         </div>
